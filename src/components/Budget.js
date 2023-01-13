e2dbecf0608261d0776fd682e392e995bdd22f5d
Vanessa Manrique <vanessamanrique99@hotmail.com>
2023-01-12 20:22:10 -0500

    Do some of the tasks
    Signed-off-by: Vanessa Manrique <vanessamanrique99@hotmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,13 +2,32 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch,expenses} = useContext(AppContext);
+
+    const changeBudget = (val)=>{
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        if (val<totalExpenses){
+            alert ("You cannot reduce the budget that is already allocated");
+        } else {
+            dispatch ({
+                type: 'SET_BUDGET',
+                payload: val,
+            })
+        }
+
+    }
 
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+        <div className='alert alert-secondary'> Budget : £
+            <input type= "number"  step="10" min= "0" max="20000" value={budget} onInput= {(event)=>changeBudget(event.target.value)}></input>
+
+            
         </div>
     );
 };
 
 export default Budget;
+
